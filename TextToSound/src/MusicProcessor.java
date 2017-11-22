96d50205d8ea4fe454d526230e31b4b43784e620
Beaterie <josef.roth@uni-weimar.de>
2017-11-22 15:28:45 +0100

    merging

---
+++ TextToSound/src/MusicProcessor.java
@@ -0,0 +1,39 @@
+
+public class MusicProcessor {
+	
+	// --------------------------------------------------------
+	// Member
+	// --------------------------------------------------------
+	
+	// <animal, index of appear>
+	private Map<String, ArrayList<Integer>> mOccurenceInfos;
+	private int duration;
+	
+	// --------------------------------------------------------
+	// Constructors
+	// --------------------------------------------------------
+		
+	public MusicProcessor() {};
+	
+	public MusicProcessor(String a, int p, int d) {
+		animal = a;
+		position = p;
+		duration = d;
+	};
+	
+	// --------------------------------------------------------
+	// music-processing methods
+	// --------------------------------------------------------
+	
+	public void process() {
+		// Pro Wort ein Takt
+		String music = "";
+		for (int i = 0; i < duration; i++) {
+			
+		}
+	}
+	
+	private void generate(int a) {
+		
+	}
+}
